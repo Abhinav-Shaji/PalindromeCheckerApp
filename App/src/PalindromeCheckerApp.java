298198096a81400101235029ebad1004f94ea6a6
Abhinav-Shaji <abhinavshaji2906@gmail.com>
2026-02-24 14:26:44 +0530

    Added UC3

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -14,5 +14,23 @@ public class PalindromeCheckerApp {
         }else{
             System.out.println("not a palindrome");
         }
+        if(isPalindrome){
+            System.out.println("it is a palindrome");
+        }else{
+            System.out.println("not a palindrome");
+        }
+        String reversed = "";
+
+// Iterate from the last character to the first
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+// Compare original and reversed string
+        isPalindrome = input.equals(reversed);
+
+// Display results
+        System.out.println("Reversed text: " + reversed);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
